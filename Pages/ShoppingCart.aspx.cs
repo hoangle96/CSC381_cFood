e144403ff5ced3a9585830c461bbac1c8cc3e80c
hoangle96 <lebahoang010296@gmail.com>
2017-11-08 14:38:39 -0500

    20171108_0239PM

--- Pages/ShoppingCart.aspx.cs
+++ Pages/ShoppingCart.aspx.cs
@@ -10,4 +10,25 @@ public partial class Pages_ShoppingCart : System.Web.UI.Page
     {
 
     }
+
+    protected void btnAddShoes_Click(object sender, EventArgs e)
+    {
+        // Add product 1 to the shopping cart
+        ShoppingCart.Instance.AddItem(1);
+
+        // Redirect the user to view their shopping cart
+        Response.Redirect("ViewCart.aspx");
+    }
+
+    protected void btnAddShirt_Click(object sender, EventArgs e)
+    {
+        ShoppingCart.Instance.AddItem(2);
+        Response.Redirect("ViewCart.aspx");
+    }
+
+    protected void btnAddPants_Click(object sender, EventArgs e)
+    {
+        ShoppingCart.Instance.AddItem(3);
+        Response.Redirect("ViewCart.aspx");
+    }
 }
