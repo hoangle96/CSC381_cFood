cff670a051f801e8d4346620622666e9f39ee5ad
Chris Vo <vochris3@gmail.com>
2017-11-27 15:07:10 -0500

    Add origin filter
    Add the origin filter into the catalog browsing page, and also take away the Verify Database page because it is just an extra page.

--- Pages/Browse.aspx.cs
+++ Pages/Browse.aspx.cs
@@ -7,10 +7,6 @@ using System.Web.UI.WebControls;
 
 public partial class Pages_Browse : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-        
-    }
 
-    
+
 }
